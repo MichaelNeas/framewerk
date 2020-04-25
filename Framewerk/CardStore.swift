d3be6ad7c2881541fade393b6574a0c8a35c6080
michaelneas <michael.r.neas@gmail.com>
2020-04-25 17:23:30 -0400

    adds shared group to share data between ios and watch app

--- Framewerk/CardStore.swift
+++ Framewerk/CardStore.swift
@@ -30,12 +30,12 @@ class CardStore {
         guard let pathComponent = component else { return paths[0] }
         return paths[0].appendingPathComponent(pathComponent.description)
     }
-
+    
     func loadData<T: Decodable>() -> [T]? {
         guard let data = try? Data(contentsOf: getDocumentsDirectory(with: .cards)) else { return nil }
         return try? decoder.decode([T].self, from: data)
     }
-
+    
     func save<T: Encodable>(data: T) {
         do {
             let data = try encoder.encode(data)
@@ -44,6 +44,30 @@ class CardStore {
             os_log("Error writing data: %s", log: Log.app, type: .error, error.localizedDescription)
         }
     }
+    
+    func sharedGroupDirectory(with component: Directory?) -> URL {
+        let path = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "framewerk")!
+        guard let pathComponent = component else { return path }
+        return path.appendingPathComponent(pathComponent.description)
+    }
+    
+    // Save to group directory for watch extensions to use the same cards created by ios App
+    func saveShared<T: Encodable>(data: T) {
+        do {
+            let data = try encoder.encode(data)
+            try data.write(to: sharedGroupDirectory(with: .cards))
+        } catch {
+            os_log("Error writing data: %s", log: Log.app, type: .error, error.localizedDescription)
+        }
+    }
+    
+    // Load from the shared group
+    func loadShared<T: Decodable>() -> [T]? {
+        guard let data = try? Data(contentsOf: sharedGroupDirectory(with: .cards)) else { return nil }
+        return try? decoder.decode([T].self, from: data)
+    }
+
+    
     
     func fetchLocalCards() -> FramewerkCardData? {
         guard let localJSONURL = Bundle.main.url(forResource: Directory.db.description, withExtension: "json"),
